dbf9f41ef8d75ce4b866698826f4decaaa80beaa
duddnddnfl <duddnddnfl@tukorea.ac.kr>
2025-06-01 20:59:07 +0900

    BaseEntity 생성 및 설정하기

---
+++ backend/src/main/java/org/example/backend/global/entity/BaseEntity.java
@@ -0,0 +1,41 @@
+package org.example.backend.global.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;    // 엔티티 최초 저장 시 자동 입력
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;    // 엔티티 수정 시 자동 갱신
+
+    @CreatedBy
+    @Column(name = "created_by")
+    private String createdBy;           // 데이터를 생성한 주체, Spring Security와 연동하여 자동 주입
+
+    @LastModifiedBy
+    @Column(name = "last_modified_by")
+    private String lastModifiedBy;      // 마지막으로 수정한 주체, 자동 주입
+
+    // soft delete 플래그
+    private boolean deleted = false;    // 실제 삭제 대신 플래그로 관리한다.
+
+}
