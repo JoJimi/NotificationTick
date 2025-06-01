06039a637a1e8e086e0312c376aaaa25f2006f46
duddnddnfl <duddnddnfl@tukorea.ac.kr>
2025-06-01 22:42:14 +0900

    BaseEntity 코드 수정하기

--- backend/src/main/java/org/example/backend/global/entity/BaseEntity.java
+++ backend/src/main/java/org/example/backend/global/entity/BaseEntity.java
@@ -26,12 +26,6 @@ public abstract class BaseEntity {
     @LastModifiedDate
     private LocalDateTime updatedAt;    // 엔티티 수정 시 자동 갱신
 
-    @CreatedBy
-    private String createdBy;           // 데이터를 생성한 주체, Spring Security와 연동하여 자동 주입
-
-    @LastModifiedBy
-    private String lastModifiedBy;      // 마지막으로 수정한 주체, 자동 주입
-
     // soft delete 플래그
     private boolean deleted = false;    // 실제 삭제 대신 플래그로 관리한다.
 
