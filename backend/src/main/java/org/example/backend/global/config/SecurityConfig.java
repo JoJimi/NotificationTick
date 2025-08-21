76abc1cdae89bc13c63cd40f9695a3e992bcc801
duddnddnfl <duddnddnfl@tukorea.ac.kr>
2025-08-21 14:18:00 +0900

    actuator 관련 Auth-WhiteList에 추가

--- backend/src/main/java/org/example/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/global/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
             "/swagger-ui/**",
             "/v3/**",
             "/oauth2/**",
-            "/auth/**"
+            "/auth/**",
+            "/actuator/**"
     };
 
     @Bean
