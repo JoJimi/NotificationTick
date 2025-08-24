34e5b985fe09cca070dec1ab6bdef3d52a943f08
duddnddnfl <duddnddnfl@tukorea.ac.kr>
2025-08-24 11:32:46 +0900

    [refactor/#33] 거래내역 관련 코드 리팩토링

--- backend/src/main/java/org/example/backend/domain/transaction/service/TransactionService.java
+++ backend/src/main/java/org/example/backend/domain/transaction/service/TransactionService.java
@@ -19,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.OffsetDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
